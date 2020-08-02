b48e3ce0da8362846866271515bf57fa7f01f027
Rodlikespants <rodwittlai@gmail.com>
2020-08-01 21:09:18 -0400

    basic hello world functionality

---
+++ client/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const HelloWorld = () => {
+  return (
+    <div>
+     <h1>Sh-sh-sh-sh!</h1>
+    </div>
+   )
+}
+
+export default hot(module)(HelloWorld) 
